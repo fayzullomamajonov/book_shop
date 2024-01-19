8a887588126eb4b49501e31ef8e03c546b1360e2
fayzullomamajonov <fmamajonov084@gmail.com>
2024-01-19 23:08:04 +0500

    admin.ModelAdmin yaratildi

--- books/admin.py
+++ books/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 from .models import BooksModel,BookAuthorModel,AuthorModel,ReviewBookModel
 # Register your models here.
 
+class InlineModel(admin.TabularInline):
+    model = BookAuthorModel
 
-admin.site.register(BooksModel)
+class BooksModelAdmin(admin.ModelAdmin):
+    list_display = ('book_name','book_price','isbn','create_at')
+    list_filter = ('book_name','create_at')
+    search_fields = ('book_name','isbn')
+    ordering = ('book_name',)
+    inlines = [InlineModel]
+
+
+
+
+admin.site.register(BooksModel,BooksModelAdmin)
 admin.site.register(BookAuthorModel)
 admin.site.register(AuthorModel)
 admin.site.register(ReviewBookModel)
--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username','first_name','last_name','email','is_active','is_staff')
+
+
+admin.site.register(CustomUser,CustomUserAdmin)
